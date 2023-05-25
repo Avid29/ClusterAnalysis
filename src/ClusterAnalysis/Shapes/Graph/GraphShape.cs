011d72c9d92d01829113cc239a2dbc2b0db5f741
Adam Dernis <Avid29@Live.com>
2023-05-25 19:50:52 -0400

    Added GraphShape

---
+++ src/ClusterAnalysis/Shapes/Graph/GraphShape.cs
@@ -0,0 +1,23 @@
+﻿// Adam Dernis 2023
+
+using ClusterAnalysis.Spaces;
+
+namespace ClusterAnalysis.Shapes.Graph;
+
+/// <summary>
+/// An <see cref="IMetricSpace{T}"/> implementation that uses a node graph to store connections.
+/// </summary>
+public class GraphShape : IMetricSpace<Node>
+{
+    /// <inheritdoc/>
+    public void AddConnection(Node node1, Node node2)
+    {
+        // Add connection bidirectionally so lookup works either way
+        node1.AddConnection(node2);
+        node2.AddConnection(node1);
+    }
+    
+    /// <inheritdoc/>
+    public bool Distance(Node a, Node b)
+        => a.IsConnected(b);
+}
---
+++ src/ClusterAnalysis/Shapes/Graph/Node.cs
@@ -0,0 +1,34 @@
+﻿// Adam Dernis 2023
+
+namespace ClusterAnalysis.Shapes.Graph;
+
+/// <summary>
+/// A point in the <see cref="GraphShape"/> space.
+/// </summary>
+public class Node
+{
+    private readonly HashSet<Node> _connections;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Node"/> class.
+    /// </summary>
+    public Node()
+    {
+        _connections = new HashSet<Node>();
+    }
+
+    /// <summary>
+    /// Adds a connection to a node.
+    /// </summary>
+    /// <param name="node">The node to connect with,</param>
+    public void AddConnection(Node node)
+        => _connections.Add(node);
+
+    /// <summary>
+    /// Gets if a node is connected to this node.
+    /// </summary>
+    /// <param name="node">The node to check for a connection with.</param>
+    /// <returns>True if the nodes are connected. False otherwise.</returns>
+    public bool IsConnected(Node node)
+        => _connections.Contains(node);
+}
