3d78d078eaa1e41e58ab8347e5b119630986e9fa
Adam Dernis <Avid29@Live.com>
2023-05-25 23:06:16 -0400

    Added SparseMatrixShape

---
+++ src/ClusterAnalysis/Shapes/Matrix/SparseMatrixShape.cs
@@ -0,0 +1,33 @@
+﻿// Adam Dernis 2023
+
+using ClusterAnalysis.Spaces;
+
+namespace ClusterAnalysis.Shapes.Matrix;
+
+/// <summary>
+/// An implementation of <see cref="IMetricSpace{T}"/> that defines relations between <see cref="MatrixCell"/>s with a sparse matrix.
+/// </summary>
+public class SparseMatrixShape : IMetricSpace<MatrixCell>
+{
+    private readonly HashSet<(MatrixCell, MatrixCell)> _connections;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SparseMatrixShape"/> class.
+    /// </summary>
+    public SparseMatrixShape()
+    {
+        _connections = new HashSet<(MatrixCell, MatrixCell)>();
+    }
+
+    /// <inheritdoc/>
+    public void AddConnection(MatrixCell node1, MatrixCell node2)
+    {
+        // Add connection bidirectionally so lookup works either way
+        _connections.Add((node1, node2));
+        _connections.Add((node2, node1));
+    }
+    
+    /// <inheritdoc/>
+    public bool Distance(MatrixCell a, MatrixCell b)
+        => _connections.Contains((a, b));
+}
