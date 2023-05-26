8b0196e70d85fd9084da2232378a8e4803172914
Adam Dernis <Avid29@Live.com>
2023-05-25 22:59:42 -0400

    Added MatrixShape

---
+++ src/ClusterAnalysis/Shapes/Matrix/MatrixCell.cs
@@ -0,0 +1,30 @@
+﻿// Adam Dernis 2023
+
+namespace ClusterAnalysis.Shapes.Matrix;
+
+/// <summary>
+/// A metric space point for the <see cref="MatrixShape"/>.
+/// </summary>
+public class MatrixCell
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MatrixCell"/> class.
+    /// </summary>
+    public MatrixCell()
+    {
+
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MatrixCell"/> class.
+    /// </summary>
+    public MatrixCell(int value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Gets the value of the node for a matrix index.
+    /// </summary>
+    public int Value { get; }
+}
---
+++ src/ClusterAnalysis/Shapes/Matrix/MatrixShape.cs
@@ -0,0 +1,39 @@
+﻿// Adam Dernis 2023
+
+using ClusterAnalysis.Spaces;
+using CommunityToolkit.Diagnostics;
+
+namespace ClusterAnalysis.Shapes.Matrix;
+
+/// <summary>
+/// An implementation of <see cref="IMetricSpace{T}"/> that defines relations between <see cref="MatrixCell"/>s with a matrix.
+/// </summary>
+public class MatrixShape : IMetricSpace<MatrixCell>
+{
+    private readonly bool[,] _connections;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MatrixShape"/> class.
+    /// </summary>
+    public MatrixShape(int size)
+    {
+        _connections = new bool[size, size];
+    }
+
+    /// <inheritdoc/>
+    public void AddConnection(MatrixCell node1, MatrixCell node2)
+    {
+        // Ensure the nodes are within the matrix size
+        var max = _connections.GetLength(0);
+        Guard.IsLessThan(node1.Value, max);
+        Guard.IsLessThan(node2.Value, max);
+
+        // Add connection bidirectionally so lookup works either way
+        _connections[node1.Value, node2.Value] = true;
+        _connections[node2.Value, node1.Value] = true;
+    }
+    
+    /// <inheritdoc/>
+    public bool Distance(MatrixCell a, MatrixCell b)
+        => _connections[a.Value, b.Value];
+}
