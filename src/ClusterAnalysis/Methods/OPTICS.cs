d3716f9f132447f181b7e0ec13ca1d1c30cf28c1
Adam Dernis <Avid29@Live.com>
2025-05-20 15:01:56 -0400

    Refactored OPTICS

--- src/ClusterAnalysis/Methods/OPTICS.cs
+++ src/ClusterAnalysis/Methods/OPTICS.cs
@@ -1,4 +1,4 @@
-﻿// Adam Dernis 2023
+﻿// Adam Dernis 2025
 
 using ClusterAnalysis.Spaces;
 
@@ -9,9 +9,6 @@ namespace ClusterAnalysis.Methods;
 /// </summary>
 public static class OPTICS
 {
-    private const int UNCLASSIFIED_ID = 0;
-    private const int NOISE_ID = -1;
-
     /// <summary>
     /// Clusters a set of points using OPTICS.
     /// </summary>
@@ -21,136 +18,156 @@ public static class OPTICS
     /// <typeparam name="T">The type of points to cluster.</typeparam>
     /// <param name="points">The list of points to cluster.</param>
     /// <param name="space">The <see cref="IMetricSpace{T}"/> defining the space.</param>
-    /// <param name="pointCount">The number of seeds needed to make a core point.</param>
-    /// <param name="maxRadius">The maximum radius allowed for a core point.</param>
+    /// <param name="minPoints">The number of seeds needed to make a core point.</param>
+    /// <param name="range">The maximum range for a point to be a neighbor.</param>
     /// <returns>A list of clusters</returns>
-    public static List<List<T>> Cluster<T>(IList<T> points, IMetricSpace<T> space, int pointCount, float maxRadius)
+    public static List<List<T>> Cluster<T>(IList<T> points, IMetricSpace<T> space, int minPoints, float range)
     {
-        // Create cluster list and context for child methods
-        var clusters = new List<List<T>>();
-        var context = new Context<T>(points, space, pointCount, maxRadius);
+        // Create context for child methods
+        var context = new Context<T>(points, space, minPoints, range);
 
         // Iterate every point
         for (int i = 0; i < points.Count; i++)
         {
-            // Skip if the point is already a member of a cluster
-            if (context.ClusterIds[i] != UNCLASSIFIED_ID)
+            // Skip if the point is already processed
+            if (context.ProcessedSet.Contains(i))
                 continue;
 
-            // Attempt to create a cluster around the point
-            if (TryCreateCluster(i, context, out var cluster))
+            if(TryUpdateCorePoint(i, context))
             {
-                clusters.Add(cluster!);
+                while (context.Seeds.Count > 0)
+                {
+                    var s = context.Seeds.Dequeue();
+                    TryUpdateCorePoint(s, context);
+                }
             }
         }
 
-        return clusters;
+        return MakeClusters(context);
     }
 
-    private static bool TryCreateCluster<T>(int index, Context<T> context, out List<T>? cluster)
+    private static bool TryUpdateCorePoint<T>(int index, Context<T> context)
     {
-        // Get the list of seeding points
-        var radius = GetSeeds(index, context, out var seeds);
+        // Process and order the point
+        int pos = context.ProcessedSet.Count;
+        context.Order[pos] = index;
+        context.ProcessedSet.Add(index);
 
-        // Check if point is not a core point
-        if (radius > context.MaxRadius || radius is float.NaN)
-        {
-            // Assign as noise
-            context.ClusterIds[index] = NOISE_ID;
+        // Get neighbors and core distance
+        var coreDist = GetCoreDistance(index, context, out var neighbors);
 
-            // Failed to create a cluster
-            cluster = null;
+        // Return if not a core point
+        if (float.IsNaN(coreDist))
             return false;
+
+        // Get the point
+        T p = context.Points[index];
+
+        foreach(var n in neighbors!)
+        {
+            // Skip processed points
+            if (context.ProcessedSet.Contains(n))
+                continue;
+
+            float newReachDist = float.Max(coreDist, context.Space.Distance(p, context.Points[n]));
+            float oldReachDist = context.Reachability[n];
+            if (float.IsPositiveInfinity(oldReachDist))
+            {
+                context.Reachability[n] = newReachDist;
+                context.Seeds.Enqueue(n, newReachDist);
+            }
+            else if (newReachDist < oldReachDist)
+            {
+                context.Reachability[n] = newReachDist;
+
+                // Just add the seed earlier in the list. It will already be processed when it pops up a second time
+                // NOTE: This is not a great solution, but the current BCL doesn't have a better priority queue
+                context.Seeds.Enqueue(n, newReachDist);
+            }
         }
 
-        // Create cluster containing core point
-        int id = context.CurrentClusterId += 1;
-        cluster = new List<T> {context.Points[index]};
-        context.ClusterIds[index] = id;
-
-        // Expand the cluster recursively
-        ExpandCluster(cluster, seeds, context);
         return true;
     }
 
-    private static void ExpandCluster<T>(List<T> cluster, List<int> seeds, Context<T> context)
+    private static float GetCoreDistance<T>(int index, Context<T> context, out List<int>? neighbors)
     {
-        // Grab the cluster id
-        int id = context.CurrentClusterId;
+        neighbors = null;
 
-        // Seeds is used as a stack for depth first search
-        while (seeds.Count > 0)
-        {
-            // Pop seed. Grab index and value
-            int s = seeds[^1];
-            T p = context.Points[s];
-            seeds.RemoveAt(seeds.Count - 1);
+        // Create a list of index of neighboring points
+        var neighborDist = new List<(int, float)>();
 
-            // Track the point's old cluster id
-            var oldId = context.ClusterIds[s];
-
-            // Skip already classified points
-            if (oldId is not (UNCLASSIFIED_ID or NOISE_ID))
-                continue;
-
-            // Add point to cluster
-            cluster.Add(p);
-            context.ClusterIds[s] = id;
-            
-            // This is an optimization because noise cannot be a core point
-            if (oldId is NOISE_ID)
-                continue;
-
-            // Get child's seeds in order to expand
-            float radius = GetSeeds(s, context, out var childSeeds);
-
-            // Check if point is a core point
-            if (radius <= context.MaxRadius)
-            {
-                // Add child seeds to stack
-                seeds.AddRange(childSeeds);
-            }
-        }
-    }
-
-    private static float GetSeeds<T>(int index, Context<T> context, out List<int> seeds)
-    {   
+        // Check every point to see if its a neighbor point for this point
         T point = context.Points[index];
-
-        // Find this point's distance from every other
-        var indexDistPairs = new List<(int, float)>();
         for (int i = 0; i < context.Points.Count; i++)
         {
-            // A point cannot be its own seed
+            // A point cannot be its own neighbor
             if (i == index)
                 continue;
 
-            T p = context.Points[i];
-            indexDistPairs.Add((i, context.Space.Distance(p, point)));
+            // Point is a neighbor if within the range
+            var distance = context.Space.Distance(point, context.Points[i]);
+            if (distance <= context.Range)
+                neighborDist.Add((i, distance));
         }
-        
-        // Filter NaN distances
-        // Sort by distance
-        // Take the nearest [PointCount] items, or fewer
-        indexDistPairs = indexDistPairs
-            .Where(x => x.Item2 is not float.NaN)
-            .OrderBy(x => x.Item2)
-            .Take(int.Min(context.PointCount, indexDistPairs.Count))
-            .ToList();
-        
-        // Get the seeds
-        // Grab indices
-        // Convert to list
-        seeds = indexDistPairs
-            .Select(x => x.Item1)
-            .ToList();
 
-        // Return NaN if there are not enough seeds
-        if (seeds.Count < context.PointCount)
+        // Return NaN if point is not a core point
+        if (neighborDist.Count < context.MinPoints)
             return float.NaN;
 
-        // Return the distance of the further seed
-        return indexDistPairs[context.PointCount-1].Item2;
+        // Sort by distance
+        neighborDist.Sort((x, y) => y.CompareTo(x));
+
+        // Get a list with just the neighbor indices.
+        neighbors = neighborDist.Select(x => x.Item1).ToList();
+
+        // Return the [min points]th distance
+        return neighborDist[context.MinPoints-1].Item2;
+    }
+
+    private static List<List<T>> MakeClusters<T>(Context<T> context)
+    {
+        var clusters = new List<List<T>>();
+
+        var cluster = new List<T>();
+        for (int o = 0; o < context.Order.Length; o++)
+        {
+            // Iterate points in order
+            int i = context.Order[o];
+            float r = context.Reachability[i];
+
+            float next = 0;
+            if (i != context.Order.Length - 1)
+                next = context.Reachability[i + 1];
+
+            float diff = r - next;
+
+            // Start a new cluster when the reachability drops by more than range
+            if (diff > context.Range)
+            {
+                // Begin next cluster
+                if (cluster.Count > 1)
+                {
+                    clusters.Add(cluster);
+                }
+
+                cluster = new List<T>();
+            }
+
+            // If the reachability is greater than range
+            if (r < context.Range || r is float.PositiveInfinity)
+            {
+                var p = context.Points[i];
+                cluster.Add(p);
+            }
+        }
+
+        // Add the in progress cluster if not empty
+        if (cluster.Count > 1)
+        {
+            clusters.Add(cluster);
+        }
+
+        return clusters;
     }
 
     private ref struct Context<T>
@@ -158,14 +175,20 @@ public static class OPTICS
         /// <summary>
         /// Initializes a new instance of the <see cref="Context{T}"/> struct.
         /// </summary>
-        public Context(IList<T> points, IMetricSpace<T> space, int pointCount, float maxRadius)
+        public Context(IList<T> points, IMetricSpace<T> space, int minPoints, float range)
         {
             Points = points;
             Space = space;
-            PointCount = int.Min(points.Count -1, pointCount);
-            MaxRadius = maxRadius;
-            ClusterIds = new int[Points.Count];
-            CurrentClusterId = 0;
+            MinPoints = int.Min(points.Count-1, minPoints);
+            Range = range;
+            Reachability = new float[points.Count];
+            Order = new int[points.Count];
+            ProcessedSet = new HashSet<int>();
+            Seeds = new PriorityQueue<int, float>();
+
+            // Initialize reachability as not a number
+            for (int i = 0; i < points.Count; i++)
+                Reachability[i] = float.PositiveInfinity;
         }
 
         /// <summary>
@@ -181,21 +204,31 @@ public static class OPTICS
         /// <summary>
         /// Gets the number of seeds needed to make a core point.
         /// </summary>
-        public int PointCount { get; }
+        public int MinPoints { get; }
+        
+        /// <summary>
+        /// Gets the max range that a point is consider a neighbor.
+        /// </summary>
+        public float Range { get; }
 
         /// <summary>
-        /// Gets the maximum radius allowed for a core point.
+        /// Gets an array mapping the order of the points.
         /// </summary>
-        public float MaxRadius { get; }
+        public int[] Order { get; }
 
         /// <summary>
-        /// Gets an array for containing the id of points in <see cref="Points"/>.
+        /// Gets an array containing the reachability distance of each point in <see cref="Points"/>.
         /// </summary>
-        public int[] ClusterIds { get; }
+        public float[] Reachability { get; }
 
         /// <summary>
-        /// Gets or sets the current cluster id.
+        /// Gets a set to track which points have been processed.
         /// </summary>
-        public int CurrentClusterId { get; set; }
+        public HashSet<int> ProcessedSet { get; }
+
+        /// <summary>
+        /// Gets a priority queue for iterating seeds.
+        /// </summary>
+        public PriorityQueue<int, float> Seeds { get; }
     }
 }
--- tests/ClusterAnalysis.Tests/OPTICSTests.cs
+++ tests/ClusterAnalysis.Tests/OPTICSTests.cs
@@ -10,26 +10,13 @@ namespace ClusterAnalysis.Tests;
 public class OPTICSTests
 {
     [TestMethod]
-    public void MaxRange5()
-    {
-        // Get points and shape
-        var space = GeneratePointsAndSpace<Node, GraphShape>(out var points);
-
-        // Cluster over the set of points
-        var clusters = OPTICS.Cluster(points, space, pointCount: 3, maxRadius: 5);
-        
-        // There is not cluster
-        Assert.AreEqual(0, clusters.Count);
-    }
-
-    [TestMethod]
     public void MaxRange7()
     {
         // Get points and shape
         var space = GeneratePointsAndSpace<Node, GraphShape>(out var points);
 
         // Cluster over the set of points
-        var clusters = OPTICS.Cluster(points, space, pointCount: 3, maxRadius: 7);
+        var clusters = OPTICS.Cluster(points, space, minPoints: 3, range: 7);
 
         // Only one cluster
         Assert.AreEqual(1, clusters.Count);
@@ -39,6 +26,7 @@ public class OPTICSTests
         var b = points[1];
         var c = points[2];
         var d = points[3];
+        var e = points[4];
         
         // Cluster contains A, B, C, and D
         var c1 = clusters[0];
@@ -47,6 +35,7 @@ public class OPTICSTests
         Assert.IsTrue(c1.Contains(b));
         Assert.IsTrue(c1.Contains(c));
         Assert.IsTrue(c1.Contains(d));
+        Assert.IsFalse(c1.Contains(e));
     }
 
     private static TShape GeneratePointsAndSpace<T, TShape>(out T[] points)
@@ -58,7 +47,8 @@ public class OPTICSTests
         var b = new T();
         var c = new T();
         var d = new T();
-        points = new[] { a, b, c, d };
+        var e = new T();
+        points = new[] { a, b, c, d, e};
 
         // Create space
         var space = new TShape();
@@ -67,6 +57,7 @@ public class OPTICSTests
         space.AddConnection(a, b, 6);
         space.AddConnection(a, c, 4);
         space.AddConnection(a, d, 4);
+        space.AddConnection(a, e, 20);
 
         return space;
     }
